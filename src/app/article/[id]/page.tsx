887cb6ece2794e0facd0136c067fbc832a0f655c
Yuuki883 <yuukisaitou14@gmail.com>
2025-04-24 00:06:33 +0900

    #30 fix paramsのid の受け取り方を修正

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -7,9 +7,11 @@ interface ArticlePageProps {
   };
 }
 
-export default function ArticlePage({ params: { id } }: ArticlePageProps) {
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  const { id } = await params;
   //npm run lint実行時にエラーが出るので、console.logを追加
   console.log(id);
+
   return (
     <main>
       <ArticleDetail />
