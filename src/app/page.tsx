b546a58391f49f92af060e5fe31d19ca0335eadf
Yoshino <kano931107@gmail.com>
2025-04-27 18:43:29 +0900

    #29 ポストカードコンポーネントを作成 (#46)
    * #29 ポストカードコンポーネントを作成
    
    * プルリク前の微修正
    
    * 型エラーの修正、不要な部分の削除など
    
    * 微修正
    
    * 不要な部分の削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,68 @@
 "use client";
 import React from "react";
 import Pagination from "./components/Pagination";
+import PostCard from "../components/PostCard";
+import "./globals.css";
 
 export default function Page() {
+  const blogPosts = [
+    {
+      post_id: "1",
+      title: "ブログタイトル",
+      user_id: "Name",
+      category_id: "category",
+      created_at: "2025-00-00",
+      updated_at: "2025-00-00",
+      content:
+        "ブログの冒頭部分ブログの冒頭部分ブログの冒頭部分ブログの冒頭部分…",
+      image_path: "/sample-thumbnail.jpg",
+    },
+  ];
+
   const handlePageChange = (page: number) => {
     console.log("選択されたページ:", page);
     // ここで API を叩いてデータ再取得、スクロールトップするなどの処理
   };
 
   return (
-    <main style={{ padding: 20 }}>
-      <Pagination totalPages={10} onPageChange={handlePageChange} />
-    </main>
+    <>
+      <div className="bg-gray-50 text-gray-900">
+        <header className="bg-white px-4 py-3">
+          <div className="max-w-5xl mx-auto flex justify-between items-center">
+            <h1 className="text-xl font-bold text-gray-900">BlogTitle</h1>
+            <nav className="space-x-4 text-sm">
+              <a
+                href="/login"
+                className="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Login
+              </a>
+              <a
+                href="/signup"
+                className="text-gray-700 bg-white hover:bg-gray-100 border border-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Sign Up
+              </a>
+            </nav>
+          </div>
+        </header>
+
+        <main>
+          <div className="max-w-6xl mx-auto flex flex-wrap gap-15 m-15">
+            {Array(9)
+              .fill(null)
+              .map((_, index) => (
+                <PostCard key={index} {...blogPosts[0]} />
+              ))}
+          </div>
+          <div style={{ padding: 20 }}>
+            <Pagination totalPages={10} onPageChange={handlePageChange} />
+          </div>
+        </main>
+        <footer className="bg-white mt-16 py-4 text-center text-sm text-gray-500">
+          © {new Date().getFullYear()} TeamDev19. All rights reserved.
+        </footer>
+      </div>
+    </>
   );
 }
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Image from "next/image";
+
+type PostCardProps = {
+  post_id: int8;
+  user_id: uuid;
+  category_id: int8;
+  title: varchar;
+  content: text;
+  image_path: text;
+  created_at: timestamp;
+  updated_at: timestamp;
+};
+
+export default function PostCard({
+  post_id,
+  user_id,
+  category_id,
+  title,
+  content,
+  image_path,
+  created_at,
+  updated_at,
+}: PostCardProps) {
+  return (
+    <Link
+      href={`/blog/${post_id}`}
+      className="block bg-white rounded-lg w-75 overflow-hidden shadow-md hover:shadow-lg transition"
+    >
+      <div className="relative w-full h-48">
+        <Image src={image_path} alt={title} fill className="object-cover" />
+      </div>
+      <div className="p-4">
+        <div className="flex justify-between items-start mb-2">
+          <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
+          <span className="text-sm text-blue-500">{category_id}</span>
+        </div>
+        <div className="text-sm">
+          <span className="mr-2 text-blue-500">{user_id}</span>
+          <span className=" text-gray-500 mr-2">{updated_at}</span>
+          <span className=" text-gray-300">{created_at}</span>
+        </div>
+        <div className="text-base mt-3 text-gray-700">
+          <span>{content}</span>
+        </div>
+      </div>
+    </Link>
+  );
+}
