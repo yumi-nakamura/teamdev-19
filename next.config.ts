051df2ac7ba7a9e668c616993b87e81f443a7bd7
yoshino <kano931107@gmail.com>
2025-05-01 10:54:30 +0900

    Profileページの作成

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://placehold.jp/50x50.png')],
+  },
 };
 
 export default nextConfig;
