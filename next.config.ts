320587e4302cf948d27ae09f8e9647eb82f35667
wakana <wknwknwkn911@gmail.com>
2025-05-15 11:22:46 +0900

    get article detail

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   protocol: 'https',
   hostname: 'placehold.jp',
   },
+  {
+        protocol: 'https',
+        hostname: 'aoevtcajtrsagxxbwwwu.supabase.co', // ← Supabase ストレージドメインを追加
+      },
   ],
   },
   };
